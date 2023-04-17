350e4e50f05fa40c5699009cc7c32ca69baf84d0
Eduardo Soares <eduardomatsuzaki@gmail.com>
2023-04-17 13:54:22 -0300

    create promoters, pdvs and allocations models and migrations

---
+++ src/database/migrations/2023_04_17_164352_create_allocations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('allocations', function (Blueprint $table) {
+            $table->id();
+            $table->string('campaign');
+            $table->string('date');
+            $table->string('alug');
+            $table->foreignId('promoter_id')->constrained();
+            $table->foreignId('pdv_id')->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('allocations');
+    }
+};
